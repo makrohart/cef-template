1770dd8623fe41e3f0a8799cc928fbb47cced4af
jini <haotian.jin@hotmail.com>
2026-02-21 19:15:57 +0800

    import bridge

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import {invoke, on, emit} from './bridge'
+import './bridge'
 
 function App() {
   const [count, setCount] = useState(0)
